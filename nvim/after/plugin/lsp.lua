7241c0a0399a86bd5250465d00ec8cb889abe557
Isaac Ruvuza <ruvuzaisaac@gmail.com>
2023-01-02 14:11:36 +0300

    disabled some 'lsp' keymaps

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,3 +1,10 @@
+-- If you need to customize mason.nvim make sure you do it before calling the lsp-zero module.
+--[[ require("mason.settings").set({
+  ui = {
+    -- border = "rounded",
+  },
+}) ]]
+
 local status_ok, lsp = pcall(require, "lsp-zero")
 if not status_ok then
   return
@@ -5,6 +12,7 @@ end
 
 lsp.preset("recommended")
 
+-- change the 'python' server to 'pyright'
 lsp.ensure_installed({
   'bashls',
   'cssls',
@@ -31,12 +39,13 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 })
 
 -- disable completion with tab
-cmp_mappings['<CR>'] = nil
 cmp_mappings['<Tab>'] = nil
 cmp_mappings['<S-Tab>'] = nil
+-- disable confirm with Enter key (use 'ctrl + y' instead)
+cmp_mappings['<CR>'] = nil
 
 lsp.setup_nvim_cmp({
-  mapping = cmp.mappings
+  mapping = cmp_mappings
 })
 
 lsp.set_preferences({
@@ -47,6 +56,7 @@ lsp.set_preferences({
     hint = '',
     info = ''
   },
+  set_lsp_keymaps = false,
 })
 
 lsp.configure("sumneko_lua", {
@@ -64,22 +74,23 @@ vim.diagnostic.config({
 })
 
 lsp.on_attach(function(client, bufnr)
+  local keymap = vim.keymap.set
   local opts = { buffer = bufnr, remap = false }
 
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-  vim.keymap.set("n", "ga", vim.lsp.buf.code_action, opts)
-  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-  vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, opts)
-  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-  vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-  vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, opts)
-  vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, opts)
-  vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
-  vim.keymap.set("n", "<leader>lf", function()
+  keymap("n", "gd", vim.lsp.buf.definition, opts)
+  keymap("n", "K", vim.lsp.buf.hover, opts)
+  keymap("n", "ga", vim.lsp.buf.code_action, opts)
+  keymap("n", "gD", vim.lsp.buf.declaration, opts)
+  keymap("n", "gT", vim.lsp.buf.type_definition, opts)
+  keymap("n", "gi", vim.lsp.buf.implementation, opts)
+  keymap("n", "gr", vim.lsp.buf.references, opts)
+  keymap("n", "<leader>dn", vim.diagnostic.goto_next, opts)
+  keymap("n", "<leader>dp", vim.diagnostic.goto_prev, opts)
+  keymap("n", "<leader>r", vim.lsp.buf.rename, opts)
+  keymap("n", "<leader>lf", function()
     vim.lsp.buf.format({ async = true })
   end, opts)
-  vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", opts)
+  keymap("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", opts)
 end)
 
 lsp.setup()
