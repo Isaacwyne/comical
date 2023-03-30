a7e779ea2160b213c3ddbf8827cee1f2777e38de
Ruvuza Isaac <ruvuzaisaac@gmail.com>
2023-03-30 08:50:36 +0300

    removed neovide

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,8 +9,3 @@ for _, source in ipairs({
     vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault)
   end
 end
-
-if vim.g.neovide then
-  vim.g.neovide_scale_factor = 1.0
-  vim.o.guifont = "Monofur Nerd Font:h12"
-end
