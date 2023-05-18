5ee8f8f5ef17304258de93c6e49b0d607d700e98
Isaac Ruvuza <ruvuzaisaac@gmail.com>
2023-05-18 12:24:42 +0300

    diagnostics icons

--- .config/nvim/after/plugin/bufferline.lua
+++ .config/nvim/after/plugin/bufferline.lua
@@ -6,13 +6,24 @@ end
 
 local opts = {
   options = {
-    diagnostics = "nvim_lsp",
-    always_show_bufferline = false,
+    always_show_bufferline = true,
     buffer_close_icon = '󰅖',
     modified_icon = '●',
     close_icon = '',
     left_trunc_marker = '',
     right_trunc_marker = '',
+    diagnostics = "nvim_lsp",
+    diagnostics_indicator = function(_, _, diag)
+      local icons = {
+        Error = " ",
+        Warn = " ",
+        Hint = " ",
+        Info = " ",
+      }
+      local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+      .. (diag.warning and icons.Warn .. diag.warning or "")
+      return vim.trim(ret)
+    end,
     offsets = {
       {
         filetype = "neo-tree",
