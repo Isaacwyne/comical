e07ed4431cbcd25257db6b5f479b3de792f15301
Isaacwyne <ruvuzaisaac@gmail.com>
2025-08-01 21:57:02 +0300

    refactored install script

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Give people a chance to retry running the installation
-trap 'echo "Omarchy installation failed! You can retry by running: source ~/.local/share/omarchy/install.sh"' ERR
-
 # Install everything
-for f in ~/.local/share/omarchy/install/*.sh; do source "$f"; done
+for f in ~/comical/install/*.sh; do source "$f"; done
 
 # Ensure locate is up to date now that everything has been installed
 sudo updatedb
