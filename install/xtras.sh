ac411f03e326c53e77b371215d394cd3dbd01167
Isaacwyne <ruvuzaisaac@gmail.com>
2025-08-01 21:55:32 +0300

    installation refactor

--- install/xtras.sh
+++ install/xtras.sh
@@ -1,7 +1,3 @@
 yay -S --noconfirm --needed \
-  signal-desktop spotify dropbox-cli zoom \
-  obsidian-bin typora libreoffice obs-studio kdenlive \
-  pinta xournalpp
-
-# Copy over Omarchy applications
-source ~/.local/share/omarchy/bin/omarchy-sync-applications || true
+  spotify obsidian-bin typora \
+  libreoffice
